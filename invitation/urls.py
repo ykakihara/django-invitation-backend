dd704423be0d50bb7912c1e759bf5aea439446e9
Daniel Barreto <dbarreto@talpor.com>
2013-05-06 12:36:29 -0430

    removing 'invitation/' from all the urls as they will be included with a common subpath.

--- invitation/urls.py
+++ invitation/urls.py
@@ -12,21 +12,18 @@ login_required_direct_to_template = login_required(TemplateView.as_view())
 
 
 urlpatterns = patterns('',
-    url(r'^invitation/$', login_required_direct_to_template,
-        {'template': 'invitation/invitation_home.html'},
-        name='invitation_home'),
-    url(r'^invitation/invite/$', InvitationView.as_view(),
+    url(r'^invite/$', InvitationView.as_view(),
         name='invitation_invite'),
-    url(r'^invitation/invite/complete/$', login_required_direct_to_template,
+    url(r'^invite/complete/$', login_required_direct_to_template,
         {'template': 'invitation/invitation_complete.html'},
         name='invitation_complete'),
-    url(r'^invitation/invite/unavailable/$', login_required_direct_to_template,
+    url(r'^invite/unavailable/$', login_required_direct_to_template,
         {'template': 'invitation/invitation_unavailable.html'},
         name='invitation_unavailable'),
-    url(r'^invitation/accept/complete/$', TemplateView.as_view(),
+    url(r'^accept/complete/$', TemplateView.as_view(),
         {'template': 'invitation/invitation_registered.html'},
         name='invitation_registered'),
-    url(r'^invitation/accept/(?P<invitation_key>\w+)/$',
+    url(r'^accept/(?P<invitation_key>\w+)/$',
         RegistrationView.as_view(), name='invitation_register'),
 )
 
@@ -36,7 +33,7 @@ if INVITE_ONLY:
         url(r'^register/$', RedirectView.as_view(),
             {'url': '../invitation/invite_only/', 'permanent': False},
             name='registration_register'),
-        url(r'^invitation/invite_only/$', TemplateView.as_view(),
+        url(r'^invite_only/$', TemplateView.as_view(),
             {'template': 'invitation/invite_only.html'},
             name='invitation_invite_only'),
     )
