cbe2a546c45ed69911549e4b586d63920e394507
Daniel Barreto <dbarreto@talpor.com>
2013-05-13 12:40:01 -0430

    using our own get_version function.

--- invitation/__init__.py
+++ invitation/__init__.py
@@ -9,9 +9,25 @@ __maintainer__ = u'Daniel Barreto'
 __email__ = 'daniel.barreto.n@gmail.com'
 __status__ = 'Beta'
 
-def get_version():
-    try:
-        from django.utils.version import get_version as django_get_version
-    except ImportError:
-        from django import get_version as django_get_version
-    return django_get_version(VERSION) # pragma: no cover
+def get_version(version=VERSION):
+    "Returns a PEP 386-compliant version number from VERSION."
+    if version is None:
+        return '0.0'
+    else:
+        assert len(version) == 5
+        assert version[3] in ('alpha', 'beta', 'rc', 'final')
+
+    # Now build the two parts of the version number:
+    # main = X.Y[.Z]
+    # sub = .devN - for pre-alpha releases
+    #     | {a|b|c}N - for alpha, beta and rc releases
+
+    parts = 2 if version[2] == 0 else 3
+    main = '.'.join(str(x) for x in version[:parts])
+
+    sub = ''
+    if version[3] != 'final':
+        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
+        sub = mapping[version[3]] + str(version[4])
+
+    return str(main + sub)
